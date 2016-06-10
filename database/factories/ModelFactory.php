c572cf2c77abdbae73bb3bd233aa34d6d8573a94
eezhal92 <arkinthesky.69@gmail.com>
2016-06-10 11:09:37 +0700

    add username column

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,8 +4,11 @@ use App\User;
 use App\Recipe;
 
 $factory->define(User::class, function (Faker\Generator $faker) {
+    $name = $faker->name;
+
     return [
-        'name' => $faker->name,
+        'name' => $name,
+        'username' => strtolower(preg_replace('/\W+/', '', substr($name, 0, rand(6, 8)))),
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
