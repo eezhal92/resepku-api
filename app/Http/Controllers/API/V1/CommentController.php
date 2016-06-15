82eacd856f06242a195ad2fe756b9ba5e7fa7161
eezhal92 <arkinthesky.69@gmail.com>
2016-06-15 13:59:59 +0700

    post comment

--- app/Http/Controllers/API/V1/CommentController.php
+++ app/Http/Controllers/API/V1/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use Illuminate\Http\Request;
 
+use JWTAuth;
 use App\Recipe;
 use App\Comment;
 use App\Http\Requests;
@@ -16,5 +17,24 @@ class CommentController extends Controller
         $recipe = Recipe::findOrFail($recipeId);
 
         return $recipe->comments()->paginate(10);
+    }
+
+    public function store(Request $request, $recipeId)
+    {
+        $this->validate($request, [
+            'body' => 'required|min:6',
+        ]);
+
+        $recipe = Recipe::findOrFail($recipeId);
+        $user = $user = JWTAuth::parseToken()->authenticate();
+
+        $comment = $recipe->comments()->create([
+            'recipe_id' => $recipeId,
+            'user_id'   => $user->id,
+            'title'     => $request->get('title', ''),
+            'body'      => $request->body,
+        ]);
+
+        return response()->json($comment, 201);
     }
 }
